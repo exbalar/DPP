4856469dca9c95bd4472daa79276b0a18ed0e1f2
Bala Subramanian Ramar <ramarbalasubramanian@gmail.com>
2018-08-16 16:50:45 +0530

    run

--- RScripts/Run.r
+++ RScripts/Run.r
@@ -1,2 +1,8 @@
-myString <- "Hello, World!"
-print ( myString)
+print ("*************Welcome to Defect Prediction Tool!!**********")
+
+system("rm -rf myApp")
+#sink("/D/hackathon/DPP/RScripts/out.txt")
+sink("D:\\hackathon\\DPP\\RScripts\\out.txt")
+output <- system("git clone https://github.com/exbalar/myApp.git", intern=TRUE, ignore.stdout=TRUE,show.output.on.console = TRUE)
+
+print(output)
